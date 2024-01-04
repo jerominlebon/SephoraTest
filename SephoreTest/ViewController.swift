5ed60faeabef775377f44bc677c814891184acef
jeromiin <jeromin.lebon@gmail.com>
2024-01-05 00:20:42 +0100

    [Project] create empty project

---
+++ SephoreTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SephoreTest
+//
+//  Created by Jeromin Lebon on 05/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
